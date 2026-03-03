1b100ef50702fbaae11854c7f1cafea5ece790d6
Pedro Lima <pedro.l.b.sousa@gmail.com>
2026-03-02 21:34:26 -0300

    fix(auth): extract client roles from keycloak resource_access

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,48 +1,9 @@
-/*import { Module } from "@nestjs/common";
-import { APP_GUARD } from "@nestjs/core";
-
-import { TestController } from "./test.controller";
-import { KeycloakJwtGuard } from "./auth/keycloak-jwt.guard";
-import { RolesGuard } from "./auth/roles.guard";
-import { PrismaModule } from "./prisma/prisma.module";
-import { PortariaModule } from "./portaria/portaria.module";
-
-@Module({
-   imports: [PrismaModule, PortariaModule],
-  controllers: [TestController],
-  providers: [
-    //{ provide: APP_GUARD, useClass: KeycloakJwtGuard },
-    //{ provide: APP_GUARD, useClass: RolesGuard },
-  ],
-})
-export class AppModule {}*/
-
-// NOVO ^^^^
-
-/*import { Module } from '@nestjs/common';
-import { APP_GUARD } from "@nestjs/core";
-import { KeycloakJwtGuard } from "./auth/keycloak-jwt.guard";
-import { RolesGuard } from "./auth/roles.guard";
-//import { AppController } from './app.controller';
-//import { AppService } from './app.service';
-//import { PrismaModule } from "./prisma/prisma.module";
-
-@Module({
-  //imports: [PrismaModule],
-  //controllers: [AppController],
-  //providers: [AppService],
-  providers: [
-    { provide: APP_GUARD, useClass: KeycloakJwtGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-  ],
-})
-export class AppModule {}*/
 import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
 
 import { TestController } from "./test.controller";
 import { KeycloakJwtGuard } from "./auth/keycloak-jwt.guard";
-import { RolesGuard } from "./auth/roles.guard";
+import { RolesGuard } from "./common/guards/roles.guard";
 import { PrismaModule } from "./prisma/prisma.module";
 import { PortariaModule } from "./portaria/portaria.module";
 
